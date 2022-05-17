68d008f7f7f4edeee48710d7f64812d3d02126a5
조경진 <cho881020@gmail.com>
2022-05-17 15:56:01 +0900

    어댑터 기초 작업 마무리

--- app/src/main/java/kr/nepp/jickbangcopy_20220517/adapters/RoomAdapter.kt
+++ app/src/main/java/kr/nepp/jickbangcopy_20220517/adapters/RoomAdapter.kt
@@ -1,7 +1,11 @@
 package kr.nepp.jickbangcopy_20220517.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import kr.nepp.jickbangcopy_20220517.R
 import kr.nepp.jickbangcopy_20220517.datas.RoomData
 
 class RoomAdapter(
@@ -10,6 +14,21 @@ class RoomAdapter(
     val mList: ArrayList<RoomData>
 ) : ArrayAdapter<RoomData>( mContext, resId, mList ) {
 
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
 
+        var tempRow = convertView
+
+        if (tempRow == null) {
+
+            tempRow = LayoutInflater.from(mContext).inflate(R.layout.room_list_item, null)
+
+        }
+
+        val row = tempRow!!
+
+
+        return row
+
+    }
 
 }
