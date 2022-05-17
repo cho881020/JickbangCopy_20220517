a0a1c19fe3671b4b2a2af2bb7e0c3ebe7363df75
조경진 <cho881020@gmail.com>
2022-05-17 16:17:14 +0900

    리스트뷰 / 어댑터 (객체화) 연결

--- app/src/main/java/kr/nepp/jickbangcopy_20220517/MainActivity.kt
+++ app/src/main/java/kr/nepp/jickbangcopy_20220517/MainActivity.kt
@@ -2,11 +2,15 @@ package kr.nepp.jickbangcopy_20220517
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kr.nepp.jickbangcopy_20220517.adapters.RoomAdapter
 import kr.nepp.jickbangcopy_20220517.datas.RoomData
 
 class MainActivity : AppCompatActivity() {
 
     val mRoomList = ArrayList<RoomData>()
+
+    lateinit var mRoomAdapter : RoomAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,5 +24,9 @@ class MainActivity : AppCompatActivity() {
         mRoomList.add(  RoomData( 256000, "서울시 은평구", -2, "6번째 방입니다." )  )
         mRoomList.add(  RoomData( 12400, "서울시 동작구", 0, "7번째 방입니다." )  )
         mRoomList.add(  RoomData( 56000, "서울시 관악구", 7, "8번째 방입니다." )  )
+
+        mRoomAdapter = RoomAdapter(this, R.layout.room_list_item, mRoomList)
+        roomListView.adapter = mRoomAdapter
+
     }
 }
