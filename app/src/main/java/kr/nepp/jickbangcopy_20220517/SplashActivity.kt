bfdc5201c81487cbc8b1f9af3bda3ae47a47a526
조경진 <cho881020@gmail.com>
2022-05-18 09:50:17 +0900

    2초 지연 처리 - Handler 사용

--- app/src/main/java/kr/nepp/jickbangcopy_20220517/SplashActivity.kt
+++ app/src/main/java/kr/nepp/jickbangcopy_20220517/SplashActivity.kt
@@ -3,6 +3,8 @@ package kr.nepp.jickbangcopy_20220517
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,11 +12,19 @@ class SplashActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash)
 
 //        2초 정도 후에 (지연 시킨 후에) 화면 이동
+
+        val myHandler = Handler( Looper.getMainLooper() )
+
+        myHandler.postDelayed(  {
+
+            val myIntent = Intent(this, MainActivity::class.java)
+            startActivity(myIntent)
+
+            finish()  // 새 화면 띄우고 현 화면 종료
+
+        },  2000  )
         
-        val myIntent = Intent(this, MainActivity::class.java)
-        startActivity(myIntent)
-        
-        finish()  // 새 화면 띄우고 현 화면 종료
+
 
     }
 }
